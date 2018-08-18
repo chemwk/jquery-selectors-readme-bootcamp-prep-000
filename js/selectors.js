178a94a20e41650f61d30fcb719b86372e1df2e3
min kim <minwookkim2013@u.northwestern.edu>
2018-08-18 20:38:12 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector (){
+  var p = $('p')
+  return p[0]
+}
+
+function lastImageSelector(){
+  return $('ul li img:last')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('div.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list li img:first-child')
+}
